cba590ffae34a470d46f865f49a9c2c488a2bb0e
ashwin232 <getashwinb4u@gmail.com>
2023-12-16 20:17:51 +0530

    Calculator

---
+++ src/components/ButtonBoxCalc.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ButtonBox.css";
+
+export const ButtonBox = ({ children }) => {
+  return (
+    <div className="buttonBox">
+      {children}
+    </div>
+  );
+};
